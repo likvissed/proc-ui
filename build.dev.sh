0b8a85f08b5d64715432d24fd47dd49d71bb4a6b
Шестаков Алексей Петрович <shestakovap@iss-reshetnev.ru>
2021-12-15 11:15:45 +0700

    Добавлен прокси для сборки контейнера

--- build.dev.sh
+++ build.dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -xe
-docker-compose -f "docker-compose.dev.yml" -p proc-ui-dev down
-DOCKER_BUILDKIT=1 docker-compose -f "docker-compose.dev.yml" -p proc-ui-dev up -d --build
+docker-compose -f "docker-compose.dev.yml" -p proc-ui-dev down --remove-orphans
+DOCKER_BUILDKIT=1 docker-compose -f "docker-compose.dev.yml" -p proc-ui-dev build  --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg no_proxy=$no_proxy
+DOCKER_BUILDKIT=1 docker-compose -f "docker-compose.dev.yml" -p proc-ui-dev up -d
